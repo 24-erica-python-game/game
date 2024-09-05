daa879a47be51b60751062c92a055af8cb81ded6
신우찬 <8621skyblue@naver.com>
2024-09-05 21:43:36 +0900

    유닛정보 칸 대략적인 배치해봄

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ GREEN = (  0, 255,   0)
 RED   = (255,   0,   0)
 
 #글자 폰트 지정
-font = pg.font.SysFont("malgungothic", 30, False, False) #(font name, size, bold, italic)
+font = pg.font.SysFont("malgungothic", 18, False, False) #(font name, size, bold, italic)
+small_font = pg.font.SysFont("malgungothic", 14, False, False)
 
 #화면 크기 지정
 size = [1280, 800]
@@ -25,9 +26,21 @@ running = True
 tile_map = TileMap()
 clock = pg.time.Clock()
 
+#TODO:임시적으로 생성한 변수로 추후에 다른 파일로 옮겨야 할듯
+active_point = 100
+player_hp = 100
+supply = 1000
+player_power = 50
+
 game_info = font.render("게임 정보", True, BLACK) #(Text, antialias,color, background color)
 minimap = font.render("미니맵", True, BLACK)
+
+#유닛정보 표시내용
 unit_info = font.render("유닛 정보", True, BLACK)
+active_point_info = font.render("활동력: {}".format(active_point), True, BLACK)
+player_hp_info = font.render("체력: {}".format(player_hp), True, BLACK)
+supply_info = font.render("보급량: {}".format(supply), True, BLACK)
+player_power_info = font.render("공격력: {}".format(player_power), True, BLACK)
 
 while running:
     clock.tick(30) # 30프레임 / 너무 높으면 CPU많이 먹으니까 10,30,60이 적당
@@ -46,7 +59,13 @@ while running:
     
     screen.blit(game_info, (640, 10))
     screen.blit(minimap, (50,700))
-    screen.blit(unit_info, (1100,700))
+
+    screen.blit(unit_info,(1150, 650))
+    pg.draw.line(screen, BLACK, (1080,675),(1280,675))
+    screen.blit(active_point_info, (1100,690))
+    screen.blit(player_hp_info, (1100,710))
+    screen.blit(supply_info, (1100,730))
+    screen.blit(player_power_info, (1100,750))
 
     pg.display.flip() #pygame의 메인 루프 끝에 반드시 사용
 print(pg.font.get_fonts())
