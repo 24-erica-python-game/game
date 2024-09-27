668ee4fdddca42fbbd9d0a538d587c50fc55eccf
DivingSeagull <chae02hb@gmail.com>
2024-09-27 12:16:38 +0900

    체크박스 구현, 예제 추가

--- src/game/ui/templates/checkbox.py
+++ src/game/ui/templates/checkbox.py
@@ -1,6 +1,7 @@
 from typing import Literal
 
-from pygame.font import FontType
+import pygame as pg
+from pygame import gfxdraw as gfx
 
 from game.ui.base import UIPosition, UISize
 from game.ui.color import RGB
@@ -9,10 +10,30 @@ from game.ui.templates.interactable import Interactable
 
 class CheckBox(Interactable):
     def __init__(self,
-                 font: FontType,
-                 label: str,
+                 default_state: bool,
                  active_color: RGB,
-                 active_indicator_type: Literal["square", "check", "cross"],
+                 active_indicator_type: Literal["square", "cross"],
                  pos: UIPosition,
                  size: UISize):
+        self.state = default_state
+        self.active_color = active_color
+        self.active_indicator_type = active_indicator_type
         super().__init__(pos, size)
+
+    def render(self):
+        display = pg.display.get_surface()
+        gfx.rectangle(display, (self.pos, self.size), RGB(70, 70, 70))
+
+        if self.state:
+            match self.active_indicator_type:
+                case "square":
+                    pg.draw.rect(display, self.active_color,
+                                 (self.pos.x + 2, self.pos.y + 2,
+                                  self.size.x - 4, self.size.y - 4))
+                case "cross":
+                    pg.draw.aaline(display, self.active_color,
+                                   (self.pos.x + 2, self.pos.y + 2),
+                                   (self.pos.x + self.size.x - 3, self.pos.y + self.size.y - 3))
+                    pg.draw.aaline(display, self.active_color,
+                                   (self.pos.x + 2, self.pos.y + self.size.y - 3),
+                                   (self.pos.x + self.size.x - 3, self.pos.y + 2))
