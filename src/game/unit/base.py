f84954778ac22c9a234800ae3da52e39df5e6945
DivingSeagull <chae02hb@gmail.com>
2024-09-27 11:43:46 +0900

    유닛 스프라이트 로딩 함수 추가

--- src/game/unit/base.py
+++ src/game/unit/base.py
@@ -1,9 +1,12 @@
+import os
+import threading
 from abc import ABCMeta, abstractmethod
-from os import PathLike
-from enum import IntEnum
 from dataclasses import dataclass
+from enum import IntEnum
+from os import PathLike
 
 from game.tile.types import Position
+
 
 class AnimationState(IntEnum):
     """
@@ -23,39 +26,95 @@ class AnimationState(IntEnum):
     MOVE = 1
     ATTACK = 2
     DEFEND = 3
-    
 
-class BaseUnit(ABCMeta):
+
+@dataclass
+class AnimationData:
+    path: PathLike
+    data: list[bytes]
+    state: AnimationState
+
+
+class BaseUnit(metaclass=ABCMeta):
     @property
     def anim_state(self) -> AnimationState:
-        return self.anim_state
+        return self.__anim_state
+
+    @anim_state.setter
+    def anim_state(self, anim_state: AnimationState):
+        self.__anim_state = anim_state
+
+    @property
+    def animations(self) -> dict[str, list[bytes]]:
+        return self.__animations
+
+    @animations.setter
+    def animations(self, animations: dict[str, list[bytes]]):
+        self.__animations = animations
 
     @property
     def action_cost(self) -> int:
-        return self.action_cost
+        return self.__action_cost
+
+    @action_cost.setter
+    def action_cost(self, action_cost: int):
+        self.__action_cost = action_cost
 
     @property
     def position(self) -> Position:
-        return self.position
+        return self.__position
+
+    @position.setter
+    def position(self, position: Position):
+        self.__position = position
 
     @property
     def supply_reserve(self) -> int:
-        return self.supply_reserve
+        return self.__supply_reserve
+
+    @supply_reserve.setter
+    def supply_reserve(self, supply_reserve: int):
+        self.__supply_reserve = supply_reserve
 
     @property
     def cost(self) -> int:
-        return self.cost
+        return self.__cost
+
+    @cost.setter
+    def cost(self, cost: int):
+        self.__cost = cost
 
     @property
     def faction(self) -> int:
-        return self.faction
+        return self.__faction
+
+    @faction.setter
+    def faction(self, faction: int):
+        self.__faction = faction
 
     @property
     def hp(self) -> int:
-        return self.hp
-    
-    def __init__(self, x: int, y: int, faction: int, hp: int=100) -> None:
+        return self.__hp
+
+    @hp.setter
+    def hp(self, hp: int):
+        self.__hp = hp
+
+    @abstractmethod
+    def __init__(self,
+                 x: int,
+                 y: int,
+                 faction: int,
+                 action_cost: int,
+                 cost: int,
+                 hp: int = 100,
+                 supply_reserve: int = 100):
+        self.anim_state = AnimationState.IDLE
+        self.animations = dict()
+        self.action_cost = action_cost
         self.position = Position(x, y)
+        self.supply_reserve = supply_reserve
+        self.cost = cost
         self.faction = faction
         self.hp = hp
 
@@ -63,6 +122,19 @@ class BaseUnit(ABCMeta):
     @abstractmethod
     def _play_animation(self, anim_path: PathLike):
         pass
+
+    def __load_animation(self, parent_dir: PathLike, anim_name: str):
+        image_dir = sorted([int(filename[:-4]) for filename in os.listdir(parent_dir)])
+        for name in image_dir:
+            with open(f"{parent_dir}/{name}.png", 'rb') as f:
+                self.animations[anim_name].append(f.read())
+                print(f"    {anim_name}: loaded image: {name}.png")
+        print(f"animation {anim_name} loaded")
+
+    def load_animations(self):
+        for (anim_name, list_path) in self.animations.items():
+            thread = threading.Thread(target=self.__load_animation, args=(list_path, anim_name))
+            thread.start()
 
     def move(self, path: list[Position]) -> bool:
         """
--- src/game/unit/unit.py
+++ src/game/unit/unit.py
@@ -1,5 +1,34 @@
-from game.unit.base import BaseUnit
+import os
+from os import PathLike
+
+from game.unit.base import BaseUnit, AnimationState
 from game.tile.types import Position
+
+
+# class TestUnit(BaseUnit):
+#     def __init__(self,
+#                  x: int,
+#                  y: int,
+#                  faction: int,
+#                  action_cost: int,
+#                  cost: int,
+#                  hp: int = 100,
+#                  supply_reserve: int = 100):
+#         self.position = Position(x, y)
+#         self.faction = faction
+#         self.action_cost = action_cost
+#         self.cost = cost
+#         self.hp = hp
+#         self.supply_reserve = supply_reserve
+#
+#         self.animations = {
+#             "test_sprite_1": {
+#                 "path": "path_to_sprites",
+#                 "data": [],
+#                 "state": AnimationState.IDLE,
+#             },
+#         }
+#         self.load_animations()
 
 
 class Ant(BaseUnit): # 개미
