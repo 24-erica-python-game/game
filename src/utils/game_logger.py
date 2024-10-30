86abf39b16d364c2533779fbb519427c03c27361
DivingSeagull <chae02hb@gmail.com>
2024-10-30 11:01:38 +0900

    게임 내용을 기록하는 ``GameLogger`` 클래스 추가

---
+++ src/utils/game_logger.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from dataclasses import dataclass
+from typing import TextIO, Optional
+
+
+log_path = os.path.normpath(f"{os.getcwd()}/data")
+
+
+@dataclass
+class PlayerData:
+    name: str
+    number: int
+
+
+class GameLogger:
+
+    """
+    게임의 내용을 기록하는 로그를 생성함.
+
+    로그는 텍스트 파일로 읽을 수 있도록 저장됨.
+    """
+    def __init__(self, player_data: list[PlayerData]):
+        """
+        :param player_data: 플레이어 데이터 목록, 정렬된 상태로 넘겨져야 함.
+        """
+        self.stream: Optional[TextIO] = None
+        self.players = player_data
+        self.block = 0
+
+    def new(self, filename: str):
+        """
+        새 게임 기록을 생성함.
+
+        로그를 읽거나 쓰려면 먼저 이 메서드를 호출해야 함.
+
+        :param filename: 로그의 이름. 마지막의 확장자는 생략해야 함.
+        :return:
+        """
+        try:
+            self.stream = open(f"{log_path}/{filename}.txt", "a")
+        except OSError as e:
+            self.stream = None
+            print(f"failed to open file: {e}", file=sys.stderr)
+
+    def write(self, content: str):
+        """
+        로그에 내용을 기록함
+
+        :return:
+        """
+        if self.stream is not None:
+            new_content = self.stream.readlines()
+            new_content.append(" " * 4 * self.block + content)
+            self.stream.writelines(new_content)
+
+    def set_block(self, level: int):
+        """
+        블록을 설정함.
+
+        (블록 1개당 공백 4개)
+
+        :param level:
+        :return:
+        """
+        self.block = level
+
+    def close(self):
+        """
+        게임 기록 파일 스트림을 닫음.
+
+        게임이 끝난 후 이 메서드를 호출해서 자원을 반납해야 함.
+
+        :return:
+        """
+        if self.stream is not None:
+            self.stream.close()
+            self.stream = None
+
+    def __del__(self):
+        if self.stream is not None:
+            try:
+                self.close()
+            except IOError:
+                self.stream = None
